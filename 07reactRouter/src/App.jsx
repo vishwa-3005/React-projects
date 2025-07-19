bb569cce77ec16b627e01550912159ef0ed7cc62
vishwadeep sankpal <vishwadeep108@gmail.com>
2025-07-20 01:20:24 +0530

    implemented react-router with in simple website

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-gray-200 bg-gray-900 text-center">React router</h1>
+    </>
+  );
+}
+
+export default App;
