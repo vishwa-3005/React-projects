ea9df97530b435c09979d31c19d44afa92a8f95c
vishwadeep sankpal <vishwadeep108@gmail.com>
2025-07-27 18:41:39 +0530

    feat: implement editing functionality for todos with update feature

--- Todo_RTK/src/features/todo/todoSlice.js
+++ Todo_RTK/src/features/todo/todoSlice.js
@@ -20,7 +20,13 @@ export const todoSlice = createSlice({
     removeTodo: (state, action) => {
       state.todos = state.todos.filter((todo) => todo.id !== action.payload);
     },
-    updateTodo: (state, action) => {},
+    updateTodo: (state, action) => {
+      const { id, newText } = action.payload;
+      const todo = state.todos.find((todo) => todo.id === id);
+      if (todo) {
+        todo.text = newText;
+      }
+    },
   },
 });
 
